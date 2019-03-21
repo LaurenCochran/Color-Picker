38a301549f2fe894afb8afc17662e8b940000059
Lauren <Owner1@MacBooks-MacBook-Pro.local>
2019-03-20 21:37:16 -0500

    Added the delegate and datasource because i forgot

--- Color Picker/ColorPickerViewController.swift
+++ Color Picker/ColorPickerViewController.swift
@@ -1,7 +1,7 @@
 
 import UIKit
 
-class ColorPickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
+class ColorPickerViewController: UIViewController {
     
     
 
@@ -15,21 +15,7 @@ class ColorPickerViewController: UIViewController, UIPickerViewDelegate, UIPicke
     
     var colors = [colorChoose(color: "red", userColor: UIColor.red), colorChoose(color: "orange", userColor: UIColor.orange), colorChoose(color:"yellow", userColor: UIColor.yellow), colorChoose(color:"green", userColor: UIColor.green), colorChoose(color:"blue", userColor: UIColor.blue), colorChoose(color:"purple", userColor: UIColor.purple)]
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1;
-    }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return colors.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        colorLabel.text = colors[row].color
-        self.view.backgroundColor = colors[row].userColor
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return colors[row].color
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -46,6 +32,25 @@ class ColorPickerViewController: UIViewController, UIPickerViewDelegate, UIPicke
     }
 
 
+}
+
+extension ColorPickerViewController:  UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        colorLabel.text = colors[row].color
+        self.view.backgroundColor = colors[row].userColor
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row].color
+    }
+    
 }
 
 
